286ac242926c6140c0f7439ca21f7514d6d5c66a
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-26 11:53:53 +0100

    Broke down main page into smaller components

---
+++ client/app/components/Main/Feedback.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feedback = () => (
+  <>
+    <section id="feedback" className="section">
+      Gimme feedback uh
+    </section>
+  </>
+);
+
+export default Feedback;
---
+++ client/app/components/Main/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => (
+  <>
+    <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg">
+      <div className="container">
+        <div className="row full-screen align-items-center">
+          <div className="col-12 col-md-12 text-center">
+            <div className="home-text">
+              <h1 className="font-alt">
+                Stock<span id="coloured">'</span>d
+              </h1>
+              <h2 className="font-alt">Easily track the metrics of your favourite companies</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+export default Home;
