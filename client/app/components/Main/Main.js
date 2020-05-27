7181a9e8790c668273fc2601ad5b49a5433eab85
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-27 08:15:39 +0100

    Removed comments

--- client/app/components/Main/Main.js
+++ client/app/components/Main/Main.js
@@ -2,10 +2,6 @@ import React from "react";
 import Home from "./Home";
 import About from "./About";
 import Feedback from "./Feedback";
-// import background from "../../../public/assets/img/background.jpg";
-// import bg2 from "../../../public/assets/img/background2.jpg";
-//           import loader from "sass-loader";
-// import { urlencoded } from 'express';
 
 const Main = () => (
   <>
