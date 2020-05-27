6fb30893afc10ceb957503623751166ec8182993
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-27 08:19:53 +0100

    Edited sign up form

--- client/app/components/Account/SignUp.js
+++ client/app/components/Account/SignUp.js
@@ -1,41 +1,165 @@
 import React, { Component } from "react";
+import "whatwg-fetch";
+import SignUpForm from "./SignUpForm";
+import Loading from "./Loading";
 
-const SignUpForm = ({ state, textChange, userAction }) => {
-  return (
-    <div>
-      {state.signUpError ? <p>{state.signUpError}</p> : null}
-      <h2>Sign Up</h2>
-      <input
-        type="text"
-        placeholder="First Name"
-        value={state.signUpFirstName}
-        onChange={textChange.onTextboxChangeSignUpFirstName}
-      />
-      <br />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={state.signUpLastName}
-        onChange={textChange.onTextboxChangeSignUpLastName}
-      />
-      <br />
-      <input
-        type="email"
-        placeholder="Email"
-        value={state.signUpEmail}
-        onChange={textChange.onTextboxChangeSignUpEmail}
-      />
-      <br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={state.signUpPassword}
-        onChange={textChange.onTextboxChangeSignUpPassword}
-      />
-      <br />
-      <button onClick={userAction.onSignUp}>Sign Up</button>
-    </div>
-  );
-};
+import { getFromStorage, setInStorage } from "../../utils/storage";
 
-export default SignUpForm;
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      token: "",
+      signUpError: "",
+      signUpFirstName: "",
+      signUpLastName: "",
+      signUpEmail: "",
+      signUpPassword: "",
+    };
+
+    this.textchange = {
+      onTextboxChangeSignUpEmail: this.onTextboxChangeSignUpEmail.bind(this),
+      onTextboxChangeSignUpPassword: this.onTextboxChangeSignUpPassword.bind(this),
+      onTextboxChangeSignUpFirstName: this.onTextboxChangeSignUpFirstName.bind(this),
+      onTextboxChangeSignUpLastName: this.onTextboxChangeSignUpLastName.bind(this)
+    };
+
+    this.useraction = {
+      onSignUp: this.onSignUp.bind(this)
+    };
+  }
+
+  componentDidMount() {
+    const obj = getFromStorage("the_main_app");
+
+    if (obj && obj.token) {
+      const { token } = obj;
+
+      // Verify the token
+      fetch("/api/account/verify?token=" + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token,
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false
+            });
+          }
+        });
+    } else {
+      this.setState({
+        isLoading: false
+      });
+    }
+  }
+
+  onSignUp() {
+    // Grab state
+    const { signUpFirstName, signUpLastName, signUpEmail, signUpPassword } = this.state;
+
+    this.setState({
+      isLoading: true
+    });
+
+    // Post request to backend
+    fetch("/api/account/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        firstName: signUpFirstName,
+        lastName: signUpLastName,
+        email: signUpEmail,
+        password: signUpPassword
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        if (json.success) {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false,
+            // Redirect them to login page rather than \/
+            signUpFirstName: "",
+            signUpLastName: "",
+            signUpEmail: "",
+            signUpPassword: ""
+          });
+        } else {
+          this.setState({
+            signUpError: json.message,
+            isLoading: false
+          });
+        }
+      });
+  }
+
+  onTextboxChangeSignUpEmail(event) {
+    this.setState({
+      signUpEmail: event.target.value
+    });
+  }
+
+  onTextboxChangeSignUpPassword(event) {
+    this.setState({
+      signUpPassword: event.target.value
+    });
+  }
+
+  onTextboxChangeSignUpFirstName(event) {
+    this.setState({
+      signUpFirstName: event.target.value
+    });
+  }
+
+  onTextboxChangeSignUpLastName(event) {
+    this.setState({
+      signUpLastName: event.target.value
+    });
+  }
+
+  render() {
+    const {
+      isLoading,
+      token,
+    } = this.state;
+
+    if (isLoading) {
+      return (
+        <>
+          <Loading/>
+        </>
+      );
+    }
+
+    if (!token) {
+      return (
+        <>
+          <div>
+            <SignUpForm
+              state={this.state}
+              textChange={this.textchange}
+              userAction={this.useraction}
+            />
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <div>
+        <p>Account</p>
+        <button onClick={this.useraction.logout}>Logout</button>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
