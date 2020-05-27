bb78f3b7ef3a3235d9e590ec96964ebeb7f1454c
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-27 08:17:09 +0100

    Created sign in form component

---
+++ client/app/components/Account/SignInForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SignInForm = ({ state, textChange, userAction }) => {
+    
+  return (
+    <div className="section">
+      {state.signInError ? <p>{state.signInError}</p> : null}
+      <h2>Sign In</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={state.signInEmail}
+        onChange={textChange.onTextboxChangeSignInEmail}
+      />
+      <br />
+      <input
+        type="password"
+        placeholder="Password"
+        value={state.signInPassword}
+        onChange={textChange.onTextboxChangeSignInPassword}
+      />
+      <br />
+      <button onClick={userAction.onSignIn}>Sign In</button>
+      <p>Don't have an account? <Link to="/signup">Sign up</Link>.</p>
+    </div>
+  );
+};
+
+export default SignInForm;
