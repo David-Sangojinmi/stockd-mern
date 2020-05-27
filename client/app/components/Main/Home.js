e3d127b8e8ed7d694cbf16144503c93086069f4e
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-27 08:16:15 +0100

    Home section restyling

--- client/app/components/Main/Home.js
+++ client/app/components/Main/Home.js
@@ -1,16 +1,16 @@
 import React from "react";
-
+// import { Image } from "react-native";
 const Home = () => (
   <>
     <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg">
       <div className="container">
         <div className="row full-screen align-items-center">
-          <div className="col-12 col-md-12 text-center">
+          <div className="col-12 col-md-12 text-left">
             <div className="home-text">
-              <h1 className="font-alt">
+              <h1>
                 Stock<span id="coloured">'</span>d
               </h1>
-              <h2 className="font-alt">Easily track the metrics of your favourite companies</h2>
+              <h2>Easily track the metrics of your favourite companies</h2>
             </div>
           </div>
         </div>
