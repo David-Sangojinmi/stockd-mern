92fd1b77cb6eb1bf97d65145efdd66cad2ca257d
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-27 22:34:37 +0100

    Added images refs

--- client/app/components/Main/Home.js
+++ client/app/components/Main/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
-// import { Image } from "react-native";
+import bg1 from "../../../public/assets/img/bg1.jpg";
+import bg2 from "../../../public/assets/img/bg2.jpg";
+import bg3 from "../../../public/assets/img/bg3.jpg";
+import bg4 from "../../../public/assets/img/bg4.jpg";
+import db1 from "../../../public/assets/img/db1.jpg";
+import db2 from "../../../public/assets/img/db2.jpg";
+import db3 from "../../../public/assets/img/db3.jpg";
+var homeStyle = {
+  backgroundImage: 'url(' + bg3 + ')'
+}
+
 const Home = () => (
   <>
-    <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg">
+    <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg" style={homeStyle}>
       <div className="container">
         <div className="row full-screen align-items-center">
-          <div className="col-12 col-md-12 text-left">
+          <div className="col-md-12 col-lg-9 text-left">
             <div className="home-text">
-              <h1>
-                Stock<span id="coloured">'</span>d
-              </h1>
+              <h1>Stock<span id="coloured">'</span>d</h1>
               <h2>Easily track the metrics of your favourite companies</h2>
             </div>
           </div>
