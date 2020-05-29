e18e46049271042c1fc399090ba7277f96c0ed4e
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-29 19:28:00 +0100

    Stock information now searchable

--- client/app/components/Account/Stock.js
+++ client/app/components/Account/Stock.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Plot from "react-plotly.js";
 import axios from "axios";
+import SearchBar from "./SearchBar";
 
 class Stock extends Component {
   constructor(props) {
@@ -9,57 +10,41 @@ class Stock extends Component {
     // this._isMounted = false;
 
     this.state = {
-      StockSymbol: "LON:AZN",
       stockChartXValues: [],
       closeYValues: [],
       openYValues: [],
       highYValues: [],
       lowYValues: [],
-      lastRefreshed: []
+      Symbol: [],
+      lastRefreshed: [],
 
       // For searchable
-      // StockTicker: null,
-      // Value: ""
+      StockTicker: null,
+      Value: ""
     };
 
-    // this.handleClick = this.handleClick.bind(this);
-    // this.handleChange = thishandleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-//   handleChange(e) {
-//     this.setState({
-//       Value: e.target.value
-//     });
-//   }
-
-//   handleClick(e) {
-//     if (e) e.preventDefault();
-//     this.setState({
-//       Value: "",
-//       StockTicker: this.state.Value
-//     });
-
-//     let StockTicker = this.state.value;
-//     const key = "F41ON15LGCFM4PR7";
-//     const url = `https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=${term}&apikey=${key}`;
-
-//     this.fetchStock();
-//   }
-
-  componentDidMount() {
-    // this._isMounted = true;
-    this.fetchStock();
+  handleChange(event) {
+    this.setState({
+      Value: event.target.value
+    });
   }
 
-  componentWillUnmount() {
-    // this._isMounted = false;
-  }
+  handleClick(event) {
+    if (event) event.preventDefault();
+    this.setState({
+      Value: "",
+      StockTicker: this.state.Value
+    });
 
-  fetchStock() {
-    const pointerToThis = this;
-    console.log(pointerToThis);
+    // let StockTicker = this.state.value;
+    // this.fetchStock();
+
     const API_KEY = process.env.API_KEY;
-    // Make stock symbol into a state that user can type in and enter
+    let StockSymbol = this.state.Value;
 
     // Make the output size toggable and then feed this as a variable
     let stockChartXValuesFunction = [];
@@ -67,35 +52,8 @@ class Stock extends Component {
     let openYValuesFunction = [];
     let highYValuesFunction = [];
     let lowYValuesFunction = [];
+    let SymbolFunction = [];
     let lastRefreshedFunction = [];
-
-    // fetch(API_CALL)
-    //   .then(function(response) {
-    //     return response.json();
-    //   })
-    //   .then(function(data) {
-    //       console.log(data);
-
-    //     for (var key in data["Time Series (Daily)"]) {
-    //       stockChartXValuesFunction.push(key);
-    //       // Adjust this to get any more data fields
-    //       closeYValuesFunction.push(data["Time Series (Daily)"][key]["4. close"]);
-    //       openYValuesFunction.push(data["Time Series (Daily)"][key]["1. open"]);
-    //       highYValuesFunction.push(data["Time Series (Daily)"][key]["2. high"]);
-    //       lowYValuesFunction.push(data["Time Series (Daily)"][key]["3. low"]);
-
-    //       // console.log(stockChartXValuesFunction);
-    //       if (this._isMounted) {
-    //         pointerToThis.setState({
-    //             stockChartXValues: stockChartXValuesFunction,
-    //             closeYValues: closeYValuesFunction,
-    //             openYValues: openYValuesFunction,
-    //             highYValues: highYValuesFunction,
-    //             lowYValues: lowYValuesFunction
-    //         });
-    //       }
-    //     }
-    //   });
 
     axios({
       method: "GET",
@@ -107,7 +65,7 @@ class Stock extends Component {
         useQueryString: true
       },
       params: {
-        symbol: this.state.StockSymbol,
+        symbol: StockSymbol, // this.state.StockSymbol
         function: "TIME_SERIES_DAILY_ADJUSTED"
       }
     })
@@ -122,7 +80,7 @@ class Stock extends Component {
           highYValuesFunction.push(response.data["Time Series (Daily)"][key]["2. high"]);
           lowYValuesFunction.push(response.data["Time Series (Daily)"][key]["3. low"]);
 
-          pointerToThis.setState({
+          this.setState({
             stockChartXValues: stockChartXValuesFunction,
             closeYValues: closeYValuesFunction,
             openYValues: openYValuesFunction,
@@ -132,14 +90,111 @@ class Stock extends Component {
         }
 
         lastRefreshedFunction.push(response.data["Meta Data"]["3. Last Refreshed"]);
-        pointerToThis.setState({
-          lastRefreshed: lastRefreshedFunction
+        SymbolFunction.push(response.data["Meta Data"]["2. Symbol"]);
+        this.setState({
+          lastRefreshed: lastRefreshedFunction,
+          Symbol: SymbolFunction
         });
       })
       .catch(error => {
         console.log(error);
       });
   }
+
+  //   componentDidMount() {
+  //     // this._isMounted = true;
+  //     this.fetchStock();
+  //   }
+
+  //   componentWillUnmount() {
+  //     // this._isMounted = false;
+  //   }
+
+  //   fetchStock() {
+  //     const pointerToThis = this;
+  //     console.log(pointerToThis);
+  //     const API_KEY = process.env.API_KEY;
+  //     // let StockSymbol = pointerToThis.state.StockSymbol;
+
+  //     // Make the output size toggable and then feed this as a variable
+  //     let stockChartXValuesFunction = [];
+  //     let closeYValuesFunction = [];
+  //     let openYValuesFunction = [];
+  //     let highYValuesFunction = [];
+  //     let lowYValuesFunction = [];
+  //     let lastRefreshedFunction = [];
+
+  //     // fetch(API_CALL)
+  //     //   .then(function(response) {
+  //     //     return response.json();
+  //     //   })
+  //     //   .then(function(data) {
+  //     //       console.log(data);
+
+  //     //     for (var key in data["Time Series (Daily)"]) {
+  //     //       stockChartXValuesFunction.push(key);
+  //     //       // Adjust this to get any more data fields
+  //     //       closeYValuesFunction.push(data["Time Series (Daily)"][key]["4. close"]);
+  //     //       openYValuesFunction.push(data["Time Series (Daily)"][key]["1. open"]);
+  //     //       highYValuesFunction.push(data["Time Series (Daily)"][key]["2. high"]);
+  //     //       lowYValuesFunction.push(data["Time Series (Daily)"][key]["3. low"]);
+
+  //     //       // console.log(stockChartXValuesFunction);
+  //     //       if (this._isMounted) {
+  //     //         pointerToThis.setState({
+  //     //             stockChartXValues: stockChartXValuesFunction,
+  //     //             closeYValues: closeYValuesFunction,
+  //     //             openYValues: openYValuesFunction,
+  //     //             highYValues: highYValuesFunction,
+  //     //             lowYValues: lowYValuesFunction
+  //     //         });
+  //     //       }
+  //     //     }
+  //     //   });
+
+  //     axios({
+  //       method: "GET",
+  //       url: "https://alpha-vantage.p.rapidapi.com/query",
+  //       headers: {
+  //         "content-type": "application/octet-stream",
+  //         "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+  //         "x-rapidapi-key": "ffe26f8252msh9f89e69954ac6b9p1164bejsn5d8cd74943f0",
+  //         useQueryString: true
+  //       },
+  //       params: {
+  //         symbol: this.state.StockSymbol, // this.state.StockSymbol
+  //         function: "TIME_SERIES_DAILY_ADJUSTED"
+  //       }
+  //     })
+  //       .then(response => {
+  //         console.log(response.data);
+
+  //         for (var key in response.data["Time Series (Daily)"]) {
+  //           stockChartXValuesFunction.push(key);
+  //           // Adjust this to get any more data fields
+  //           closeYValuesFunction.push(response.data["Time Series (Daily)"][key]["4. close"]);
+  //           openYValuesFunction.push(response.data["Time Series (Daily)"][key]["1. open"]);
+  //           highYValuesFunction.push(response.data["Time Series (Daily)"][key]["2. high"]);
+  //           lowYValuesFunction.push(response.data["Time Series (Daily)"][key]["3. low"]);
+
+  //           pointerToThis.setState({
+  //             stockChartXValues: stockChartXValuesFunction,
+  //             closeYValues: closeYValuesFunction,
+  //             openYValues: openYValuesFunction,
+  //             highYValues: highYValuesFunction,
+  //             lowYValues: lowYValuesFunction
+  //           });
+  //         }
+
+  //         lastRefreshedFunction.push(response.data["Meta Data"]["3. Last Refreshed"]);
+  //         pointerToThis.setState({
+  //           lastRefreshed: lastRefreshedFunction
+  //         });
+  //       })
+  //       .catch(error => {
+  //         console.log(error);
+  //       });
+  //   }
 
   //   onStockSymbolChange(event) {
   //     this.setState({
@@ -154,6 +209,8 @@ class Stock extends Component {
   //
 
   render() {
+    const Value = this.state.Value;
+
     return (
       <>
         <div className="container">
@@ -165,12 +222,13 @@ class Stock extends Component {
               <p>FB</p>
               <p>GOOG</p>
               <p>
-                <strong>Current:</strong> <span id="coloured">{this.state.StockSymbol}</span>
+                <strong>Current:</strong> <span id="coloured">{this.state.Symbol}</span>
               </p>
             </div>
             <div className="col-md-12 col-lg-8 gray-bg-1">
               <h4>Analyse Stock Data</h4>
-              <h5>Symbol: {this.state.StockSymbol}</h5>
+              <SearchBar value={Value} onChange={this.handleChange} onClick={this.handleClick} />
+              <h5>Symbol: {this.state.Symbol}</h5>
               <p>Last refreshed: {this.state.lastRefreshed}</p>
               <Plot
                 data={[
