feae71811922a0edf4b1bfd6ff7367f8d1360481
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-05-29 08:01:18 +0100

    Create api.js

---
+++ client/app/components/Account/api.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+
+axios({
+  method: "GET",
+  url: "https://alpha-vantage.p.rapidapi.com/query",
+  headers: {
+    "content-type": "application/octet-stream",
+    "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+    "x-rapidapi-key": "0fb5849632msh27d91b548a4ae62p183afdjsnffa5dd4d941c",
+    useQueryString: true
+  },
+  params: {
+    outputsize: "compact",
+    symbol: symbol.toUpperCase(),
+    function: "TIME_SERIES_DAILY_ADJUSTED"
+  }
+})
+  .then(response => {
+    console.log(response);
+    return response;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+// const instance = axios.create({
+//   baseURL: "https://alpha-vantage.p.rapidapi.com",
+//   headers: {
+//     "content-type": "application/octet-stream",
+//     "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+//     "x-rapidapi-key": "NJQON9T3NI17YNY1"
+//   }
+// });
+
+// export default {
+//   stockTimeSeries: symbol =>
+    // instance({
+    //   method: "GET",
+    //   url: "/query",
+    //   params: {
+    //     outputsize: "compact",
+    //     datatype: "json",
+    //     function: "TIME_SERIES_DAILY_ADJUSTED",
+    //     symbol: symbol.toUpperCase()
+    //   },
+    //   transformResponse: [
+    //     function(data) {
+    //       // Do whatever you want to transform the data
+    //       console.log("Transforming data...");
+
+    //       const json = JSON.parse(data);
+
+    //       const dates = Object.keys(json["Time Series (Daily)"]).reverse;
+
+    //       // Construct response data for chart input
+    //       const closePrices = dates.map(
+    //         date =>
+    //           (date = {
+    //             date,
+    //             close: Number(json["Time Series (Daily)"][date]["4. close"])
+    //           })
+    //       );
+
+    //       const symbol = json["Meta Data"]["2. Symbol"];
+    //       const refreshed = json["Meta Data"]["3. Last Refreshed"];
+
+    //       data = {
+    //         symbol,
+    //         refreshed,
+    //         closePrices
+    //       };
+
+    //       return data;
+    //     }
+    //   ]
+    // })
+// };
