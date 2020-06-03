f60e2910d1f35524c3d66a48efb59745cffdd1e7
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-06-04 00:08:41 +0100

    Updating styling

--- client/app/components/Account/Loading.js
+++ client/app/components/Account/Loading.js
@@ -3,9 +3,17 @@ import React from "react";
 const Loading = () => {
 
     return (
-      <div className="loadingDiv">
-        <p>Loading...</p>
-      </div>
+      <>
+        <section className="section gray-bg-1">
+          <div className="container">
+            <div className="row">
+              <div className="loadingDiv">
+                <p>Loading...</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
     );
 }
 
