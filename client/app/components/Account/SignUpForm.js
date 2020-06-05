cd3a7333619a7c0070faf59533ad519a210330e6
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-06-06 00:21:40 +0100

    Redesigned sign up page

--- client/app/components/Account/SignUpForm.js
+++ client/app/components/Account/SignUpForm.js
@@ -3,40 +3,58 @@ import { Link } from "react-router-dom";
 
 const SignUpForm = ({ state, textChange, userAction }) => {
   return (
-    <div className="section">
+    <div className="centered">
       {state.signUpError ? <p>{state.signUpError}</p> : null}
-      <h2>Sign Up</h2>
-      <input
-        type="text"
-        placeholder="First Name"
-        value={state.signUpFirstName}
-        onChange={textChange.onTextboxChangeSignUpFirstName}
-      />
-      <br />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={state.signUpLastName}
-        onChange={textChange.onTextboxChangeSignUpLastName}
-      />
-      <br />
-      <input
-        type="email"
-        placeholder="Email"
-        value={state.signUpEmail}
-        onChange={textChange.onTextboxChangeSignUpEmail}
-      />
-      <br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={state.signUpPassword}
-        onChange={textChange.onTextboxChangeSignUpPassword}
-      />
-      <br />
-      <button onClick={userAction.onSignUp}>Sign Up</button>
+      <h1>Sign Up<span id="coloured">.</span></h1>
+      <p>Hello! Enter your details to start tracking your favourite companies today.</p>
+      <div className="form-group">
+        <input
+          className="form-control sign-group"
+          type="text"
+          placeholder="First Name"
+          value={state.signUpFirstName}
+          onChange={textChange.onTextboxChangeSignUpFirstName}
+        />
+        <span className="sign-input-focus-effect"></span>
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control sign-group"
+          type="text"
+          placeholder="Last Name"
+          value={state.signUpLastName}
+          onChange={textChange.onTextboxChangeSignUpLastName}
+        />
+        <span className="sign-input-focus-effect"></span>
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control sign-group"
+          type="email"
+          placeholder="Email"
+          value={state.signUpEmail}
+          onChange={textChange.onTextboxChangeSignUpEmail}
+        />
+        <span className="sign-input-focus-effect"></span>
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control sign-group"
+          type="password"
+          placeholder="Password"
+          value={state.signUpPassword}
+          onChange={textChange.onTextboxChangeSignUpPassword}
+        />
+        <span className="sign-input-focus-effect"></span>
+      </div>
+      <button className="m-btn-sign m-btn-theme" onClick={userAction.onSignUp}>
+        ->Create Account
+      </button>
       <p>
-        Made an account? <Link to="/account/signin">Sign in</Link>.
+        Already have an account? Go to{" "}
+        <span id="coloured"><Link to="/account/signin">
+          sign in</Link></span>
+        .
       </p>
     </div>
   );
