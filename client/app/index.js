48b64327c2c0b58a68b140c47a64763e85ab27a7
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-06-06 00:24:25 +0100

    Added new routes

--- client/app/index.js
+++ client/app/index.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import App from './components/App/App';
-import NotFound from './components/App/NotFound';
+import App from "./components/App/App";
+import NotFound from "./components/App/NotFound";
 
-import Main from './components/Main/Main';
-import AccountHome from './components/Account/AccountHome';
-import SignUp from './components/Account/SignUp';
+import Main from "./components/Main/Main";
+import AccountHome from "./components/Account/AccountHome";
+import SignIn from "./components/Account/SignIn";
+import SignUp from "./components/Account/SignUp";
 
-import './styles/styles.scss';
-import './styles/color/default.scss';
+import "./styles/styles.scss";
+import "./styles/color/default.scss";
 
-render((
+render(
   <Router>
     <App>
       <Switch>
-        <Route exact path="/" component={Main}/>
-        <Route path="/account/signin" component={AccountHome}/>
-        <Route path="/account/signup" component={SignUp}/>
-        <Route component={NotFound}/>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/account/home" component={AccountHome} />
+        <Route exact path="/account/signin" component={SignIn} />
+        <Route exact path="/account/signup" component={SignUp} />
+        <Route component={NotFound} />
       </Switch>
     </App>
-  </Router>
-), document.getElementById('app'));
+  </Router>,
+  document.getElementById("app")
+);
