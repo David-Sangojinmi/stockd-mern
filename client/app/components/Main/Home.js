0bd7c0e3e300f62694859cef58390f20629d722d
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-06-06 00:25:41 +0100

    Commented unused bgs

--- client/app/components/Main/Home.js
+++ client/app/components/Main/Home.js
@@ -1,18 +1,18 @@
 import React from "react";
 import bg1 from "../../../public/assets/img/bg1.jpg";
-import bg2 from "../../../public/assets/img/bg2.jpg";
-import bg3 from "../../../public/assets/img/bg3.jpg";
-import bg4 from "../../../public/assets/img/bg4.jpg";
-import db1 from "../../../public/assets/img/db1.jpg";
-import db2 from "../../../public/assets/img/db2.jpg";
-import db3 from "../../../public/assets/img/db3.jpg";
+// import bg2 from "../../../public/assets/img/bg2.jpg";
+// import bg3 from "../../../public/assets/img/bg3.jpg";
+// import bg4 from "../../../public/assets/img/bg4.jpg";
+// import db1 from "../../../public/assets/img/db1.jpg";
+// import db2 from "../../../public/assets/img/db2.jpg";
+// import db3 from "../../../public/assets/img/db3.jpg";
 var homeStyle = {
-  backgroundImage: 'url(' + bg3 + ')'
+  backgroundImage: 'url(' + bg1 + ')'
 }
 
 const Home = () => (
   <>
-    <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg" style={homeStyle}>
+    <section id="home" className="home-banner-01 bg-no-repeat bg-cover bg-fixed home-bg tint-bg-1" style={homeStyle}>
       <div className="container">
         <div className="row full-screen align-items-center">
           <div className="col-md-12 col-lg-9 text-left">
