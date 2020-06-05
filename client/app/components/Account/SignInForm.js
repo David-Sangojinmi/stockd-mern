21947e4d70c09385b466b4b8d3c7ed302a83b6bd
David-Sangojinmi <dsangojinmi26@gmail.com>
2020-06-06 00:21:29 +0100

    Redesigned sign in page

--- client/app/components/Account/SignInForm.js
+++ client/app/components/Account/SignInForm.js
@@ -2,27 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SignInForm = ({ state, textChange, userAction }) => {
-    
   return (
-    <div className="section">
+    <div className="centered">
       {state.signInError ? <p>{state.signInError}</p> : null}
-      <h2>Sign In</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={state.signInEmail}
-        onChange={textChange.onTextboxChangeSignInEmail}
-      />
-      <br />
-      <input
-        type="password"
-        placeholder="Password"
-        value={state.signInPassword}
-        onChange={textChange.onTextboxChangeSignInPassword}
-      />
-      <br />
-      <button onClick={userAction.onSignIn}>Sign In</button>
-      <p>Don't have an account? <Link to="/account/signup">Sign up</Link>.</p>
+      <h1>Sign In<span id="coloured">.</span></h1>
+      <p>Sign in to access your dashboard and get back to tracking.</p>
+      <div className="form-group">
+        <input
+          className="form-control sign-group"
+          type="email"
+          placeholder="Email"
+          value={state.signInEmail}
+          onChange={textChange.onTextboxChangeSignInEmail}
+        />
+        <span className="sign-input-focus-effect"></span>
+      </div>
+      <div className="form-group">
+        <input
+          className="form-control sign-group"
+          type="password"
+          placeholder="Password"
+          value={state.signInPassword}
+          onChange={textChange.onTextboxChangeSignInPassword}
+        />
+        <span className="sign-input-focus-effect"></span>
+      </div>
+      <button className="m-btn-sign m-btn-theme" onClick={userAction.onSignIn}>
+        -> Continue
+      </button>
+      <p>
+        Haven't created an account yet? Sign up{" "}
+        <span id="coloured"><Link to="/account/signup">
+          here</Link></span>
+        .
+      </p>
     </div>
   );
 };
