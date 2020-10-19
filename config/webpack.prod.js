1f2ca01ca22eb0b6bed3d23079152eb8e263bec5
David Sangojinmi <dsangojinmi26@gmail.com>
2020-10-19 15:57:59 +0100

    Installed UglifyJs Webpack Plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,15 +7,15 @@ const commonConfig = require("./webpack.common");
 module.exports = merge(commonConfig, {
   mode: "production",
 
-  optimization: {
-    minimize: false
-  },
-  
   output: {
     filename: "js/[name].[hash].js",
     chunkFilename: "[id].[hash].chunk.js"
   },
 
+  optimization: {
+    minimize: false
+  },
+  
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
